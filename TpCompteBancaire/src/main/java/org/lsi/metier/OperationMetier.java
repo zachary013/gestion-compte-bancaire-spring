38a35d0671dd8a12c6fee4901bf850760fd0f3be
Zakariae Azn <azarkanzakariae@gmail.com>
2024-11-07 22:23:30 +0100

    Operation Business Logic

--- TpCompteBancaire/src/main/java/org/lsi/metier/OperationMetier.java
+++ TpCompteBancaire/src/main/java/org/lsi/metier/OperationMetier.java
@@ -1,8 +1,13 @@
 package org.lsi.metier;
+
 import org.lsi.entities.Operation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.util.List;
 
 public interface OperationMetier {
-    public Operation saveOperation(Operation op);
-    public List<Operation> listOperations();
+    Operation saveOperation(Operation op);
+    List<Operation> listOperation();
+    Page<Operation> listOperationCompte(String codeCompte, Pageable pageable);
 }
